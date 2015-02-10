016c10ae29a22c2b4852cb177fd41baa84d47ce0
Kyle Samson <kylesamson21@gmail.com>
2015-02-10 10:20:03 -0400

    added more details to basics playground

--- playgrounds/Basics.playground/section-1.swift
+++ playgrounds/Basics.playground/section-1.swift
@@ -6,9 +6,14 @@ import UIKit
 // String Variable - Inferred
 //var (mutable)
 var string1 = "Hello, my name is"
+
 //OR - Explicit - type annotation
-var string2: String = "Hello, my name is"
-var name: String = "Tiffany"
+// Forcing the variable to be a certain type of data by using the : followed by the data type.
+var strongName:String = "LESTER"
+var strongInt:Int = 78
+var string2:String = "Hello, my name is"
+var name:String = "Tiffany"
+
 // Integer Variable
 var num: Int = 3
 var num1: Int = 5
@@ -19,15 +24,11 @@ var num3 = 100
 let myName = "Kyle"
 let myName2: String = "Thomas"
 
-// Concatination & String Interpolation (sp?)
+// Concatination & String Interpolation
 var str = string1 + " " + name + "!"
 var str1 = "\(string1) \(name)!"
 var str2 = "The value of num2 is: \(num2)."
 var str3 = "Here are all of my variables: \(string1), \(name), \(num), \(num1), \(num2) & \(num3)!"
-
-// Forcing the variable to be a certain type of data by using the : followed by the data type.
-var strongName:String = "LESTER"
-var strongInt:Int = 78
 
 // Basic Math Examples
 var output = "Your score is: \(num * num3)"
@@ -37,95 +38,110 @@ var output2 = "Division example: \(num2/num1)!"
 
 // **type of values you can store in arrays and dictionaries cannot change once the collection is declared.**
 
-// 1 : ARRAYS
+// ---1 : ARRAYS---
+
 // var colors: [String] = ["Jack", "Queen", "King"]
 var colors = ["blue", "red", "green", "yellow", "purple"]
 var team2 = colors[1]
+
 //Combining two Immutable arrays - MUST BE THE SAME TYPE
 let a = [1, 2, 3]
 let b = [4, 5, 6]
-
 let c = a + b
+
 //Appending to a MUTABLE ARRAY
 var array = [1, 2, 3]
 array += [4]
 println(array)
-//Array Operations
+
+//Array Operations - Append, Count & Insert
 array.append(5)
 array.count
 array.insert(6, atIndex: 0)
 
 
-// 2 : DICTIONARIES
+// ---2 : DICTIONARIES---
 //Similar to Hash Table - Store key value pairs - Access the value by providing the key
+
 //card names are the keys and the numbers are their values
 var cards = ["Jack": 11, "Queen": 12, "King": 13]
+
 //adding to the Dictionary
 cards["Ace"] = 14
+
 //change a value
 cards["Ace"] = 1
 println(cards["Ace"])
-//Dictionary Operationals
+
+//Dictionary Operationals - RemoveValueForKey
 cards.removeValueForKey("Jack")
 println(cards)
 
-// 3(?) : TUPLES - Differ from Arrays and Dictionaries because they can contain values that are NOT of the same type
+// ---3 : TUPLES--- Differ from Arrays and Dictionaries because they can contain values that are NOT of the same type
+
 //tuple with type string and double
 var currency = ("EUR", 0.81)
+
 //tuple with NSDate instance and string literal
 var time = (NSDate(), "This is my message.")
+
 //tuple with String String type
-var email = ("Bart Jacobs", "bart@example.com")
+var email = ("Bart Jacobs", "bart@example.com", 15678)
+
 //Accessing Values - Zero Based
 var rate = currency.1
 var message = time.1
 var emailAddress = email.0
+var emailNumber = email.2
+
 //Increase Readability -> Name Values in Tuple
 var person = (name: "Kyle", age: 25)
 let personName = person.name
 let personAge = person.age
+
 //Decomposition
 let(currencyName, currencyRate) = currency
 println("\(currencyName) and \(currencyRate)")
+
 //use _ (underscore) to tell Swift which values you are NOT interested in
 let (currencyName2, _) = currency
 println(currencyName2)
 
 //-------------------OPTINALS AND CONTROL FLOW--------------------
 
-//Optionals - use of ? - used to encapsulate
+// ---1 : OPTIONALS--- use of ? - used to encapsulate
 //given default value of nil - IMPORTANT TO NOTE : nil means the absence of a value, any value - used for any type.
 //Important to note that word is not of type String but it is of type Optional
 var word: String?
+
 //Optional Binding = Safer way to access value of the Optional - Ensures word won't be force unwrapped accidently
 if word != nil{
+    //force unwrap it - add a ! to the end of the variable - need to make sure it has a value or an error will be thrown
     println(word!)
 }else{
     println("word has no value!")
 }
 word = "Example of an optional"
-//force unwrap it - add a ! to the end of the variable - need to make sure it has a value or an error will be thrown
-println(word!)
-if word != nil{
+
+if let thereIsAWord = word?{
     println(word!)
 }else{
     println("word has no value!")
 }
+
 //Optional Binding with a temporary Constant
 //“If the optional Int returned by possibleNumber.toInt contains a value, set a new constant called actualNumber to the value contained in the
 //optional.”
 //If the conversion is successful, the actualNumber constant becomes available for use within the first branch of the if statement.
 //It has already been initialized with the value contained within the optional, and so there is no need to use the ! suffix to access its value.
-//In this example, actualNumber is simply used to print the result of the conversion.
 let possibleNumber = "123"
 if let actualNumber = possibleNumber.toInt() {
     println("\'\(possibleNumber)\' has an integer value of \(actualNumber)")
 } else {
     println("\'\(possibleNumber)\' could not be converted to an integer")
 }
-// prints "'123' has an integer value of 123"
 
-//CONTROL FLOW
+// ---2 : CONTROL FLOW---
 
 //LOOPS
 //While Loop
@@ -140,14 +156,22 @@ if let actualNumber = possibleNumber.toInt() {
 //    message = "Downloading..."
 // } while !complete
 
+//***parentheses are optional - NEAT!***
+
 //For-in Loops
 for cardName in cards{
     println(cardName)
 }
-//For-in Loops with range
+
+//For-in Loops Using Ranges - Important to use 3 .'s
 for number in 1...10 {
     println("\(number) times 2 is \(number*2)")
 }
+
+for number in 1..<10 {
+    println("\(number) times 2 is \(number*2)")
+}
+
 //Printing out both Key and Value
 for (cardName, cardValue) in cards {
     println("\(cardName) = \(cardValue)")
@@ -173,10 +197,11 @@ if(fruit == myFruit){
 }else{
     println("No conditions met!")
 }
-//parentheses are optional - NEAT!
+
 if  fruit != myFruit {
     println("Not Equal")
 }
+
 //Switch Case Statement
 switch fruit{
     case "apple":
@@ -186,7 +211,8 @@ switch fruit{
     default:
         println("No criteria met.")
 }
-//Using Ranges - Important to use 3 .'s
+
+//Using Ranges
 var distance = 44
 switch distance {
 case 0:
@@ -202,6 +228,7 @@ default:
     println("too far")
     
 }
+
 //Value Binding - second value of the Tuple is temporarily bound to the constant description
 var response = (200, "OK")
 
@@ -212,6 +239,18 @@ case (400..<500, let description):
     println("The request was unsuccessful with description \(description).")
 default:
     println("The request was unsuccessful with no description.")
+}
+
+var tuple3 = (450, "Do not care about me", "You care about me")
+
+switch tuple3 {
+    case (0...449,_,let message):
+        println("I should not get in here.")
+    case (450..<500,_,let message):
+        println("If I got into here, you are smart. Pat yourself on the back. \(message)")
+        println("This is another way to write message : \(tuple3.2)")
+    default:
+        println("You are dumb. Nothing matches.")
 }
 
 //----------------------FUNCTIONS-------------------
@@ -228,16 +267,22 @@ func getNames() -> (String, String){
     return ("Name 1", "Name 2")
 }
 getNames()
+
 //function with DEFAULT VALUE for second parameter
+//Swift provides an automatic external name for any parameter that has a default value. 
+//The automatic external name is the same as the local name, 
+//as if you had written a hash symbol before the local name in your code.
 func printDate(date: NSDate, format: String = "YY/MM/dd")-> String{
     let dateFormatter = NSDateFormatter()
     dateFormatter.dateFormat = format
     return dateFormatter.stringFromDate(date)
 }
 printDate(NSDate())
-//changing second parameter - need to tell Swift the parameters name
+
+//changing second parameter - ***Need to tell Swift the parameters name***
 printDate(NSDate(), format: "dd/MM//YY")
-//function returning three values (tuple) - labeling the values of the tuples returned
+
+//function returning three values - labeling the values of the tuples returned
 func timeComponentsForDate(date: NSDate) -> (hour: Int,minute: Int,second: Int) {
     let dateComponents = NSCalendar.currentCalendar().components((.CalendarUnitHour | .CalendarUnitMinute | .CalendarUnitSecond), fromDate: date)
     let hour = dateComponents.hour
@@ -250,7 +295,21 @@ let timeComponents = timeComponentsForDate(NSDate())
 println(timeComponents.hour)
 println(timeComponents.minute)
 println(timeComponents.second)
+
 //function with external names
+func externalNameExample(firstString string1: String, secondString string2: String, joinedString string3: String) -> String{
+    
+    return "Here is the final string: \(string1) \(string3) \(string2)"
+}
+externalNameExample(firstString: "what", secondString: "is up?", joinedString: "the heck")
+
+//shorthand external/local paramet naming
+func shorthandExternalNameExample(#firstString: String, #secondString: String, #joinedString: String) -> String{
+    
+    return "Here is the final string: \(firstString) \(joinedString) \(secondString)"
+}
+shorthandExternalNameExample(firstString: "what", secondString: "is up?", joinedString: "the what the")
+
 func power(base a: Int, exponent b: Int) -> Int{
     var result = a
     for _ in 1..<b{
@@ -259,6 +318,20 @@ func power(base a: Int, exponent b: Int) -> Int{
     return result
 }
 power(base: 2, exponent: 3)
+
+//Variadic Parameters - accepts zero or more values of a specified type.
+//Write variadic parameters by inserting three period characters (...) after the parameter’s type name.
+//The values passed to a variadic parameter are made available within the function’s body as an array of the
+//appropriate type.
+func addEmUp(numbers: Double...) -> Double {
+    var total: Double = 0
+    for number in numbers {
+        total += number
+    }
+    return total
+}
+addEmUp(1, 2, 3, 4, 5)
+addEmUp(3, 8.25, 18.75)
 
 //-----------------------CLOSURES---------------------
 
